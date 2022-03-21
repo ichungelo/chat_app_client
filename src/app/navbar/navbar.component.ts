4c7b13e099feffd22868b19fad3d321783f5381b
Krisna Satriadi <krisna.s.362@gmail.com>
2022-03-21 13:22:38 +0700

    add logout button

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
+import { Token } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  buttonLogout: string = "Logout"
 
-  constructor() { }
+  constructor( private userService: UserService) { }
+
+  isLogin: boolean = false
+
+  token = localStorage.getItem("token")
 
   ngOnInit(): void {
+    if(this.token == null) {
+      this.isLogin = false
+    } else {
+      this.isLogin = true
+    }
   }
 
+  logoutUser() {
+    localStorage.removeItem("token")
+    window.location.href = "/login"
+    alert("Logout success")
+  }
 }
